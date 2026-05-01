727c2f7974b9bbc76c91f7e4346b844141c2b297
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-01 11:38:48 +0500

    Learn about .env

--- Basic Server/index.js
+++ Basic Server/index.js
@@ -1,30 +1,31 @@
 const express = require('express');
+require('dotenv').config()
 
 const app = express();
 
-const port = 3000
+// const port = 3000
 
 //Making Routes
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {  //This is Home Route
     res.send("Hello World!")
 }) //by this method we can send get request on any route
 
-// app.get('/waris' , (req , res) => {
-//     res.send("WarisCodes")
-// })
+app.get('/waris' , (req , res) => {
+    res.send("<h1>WarisCodes</h1>")
+})
 
 app.get('/login', (req, res) => {
     res.send("Login With Google")
 })
 
 app.get('/about', (req, res) => {
-    res.send("Welcome To About us")
+    res.send("<h2>Welcome To About us</h2>")
 })
 
 //Now listen to the port to make our server 
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`App Listening on port ${port}`)
 })
 
