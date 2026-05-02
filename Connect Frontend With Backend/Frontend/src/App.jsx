d034ed5fa95e68005dfb33e4177249781b151501
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-02 12:19:19 +0500

    Make React app to Display the data on frontend

---
+++ Connect Frontend With Backend/Frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  return (
+    <>
+   <h1>Learning To connect Backend with Frontend</h1>
+   <p>JOKES: {jokes.length}</p>
+
+   {
+    jokes.map((joke , index) => {
+      <div key={joke.id}> \
+      <h3>{joke.title}</h3>
+      <p>{joke.content}</p>
+      </div>
+      
+    })
+   }
+    </>
+  )
+}
+
+export default App
