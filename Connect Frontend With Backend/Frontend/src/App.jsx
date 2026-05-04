c43110abaa9cf22975651a14d1f19447a5a15bb6
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-04 21:05:07 +0500

    Learn About CORS

--- Connect Frontend With Backend/Frontend/src/App.jsx
+++ Connect Frontend With Backend/Frontend/src/App.jsx
@@ -1,23 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [jokes, setJokes] = useState([])
 
+  useEffect(() => {
+    axios.get('http://localhost:5000/jokes') // if using axios then dont have to do .JSON it automaticaaly do that
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  })
+  //Just Writing this code won't connect it with beackend , this will give CORS error
+//   CORS (Cross-Origin Resource Sharing) is a browser security mechanism that blocks requests made from one origin to a different origin — unless the server explicitly allows it.
+// What is an "Origin"?
+// An origin is the combination of protocol + domain + port:
+
+
   return (
     <>
-   <h1>Learning To connect Backend with Frontend</h1>
-   <p>JOKES: {jokes.length}</p>
+      <h1>Learning To connect Backend with Frontend</h1>
+      <p>JOKES: {jokes.length}</p>
 
-   {
-    jokes.map((joke , index) => {
-      <div key={joke.id}> 
-      <h3>{joke.title}</h3>
-      <p>{joke.content}</p>
-      </div>
-      
-    })
-   }
+      {
+        jokes.map((joke, index) => {
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+
+        })
+      }
     </>
   )
 }
