57f8fb867a042a5dcbcbb0678d269f388c7ede5c
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-07 23:51:16 +0500

    Small Fixes

--- Data Modelling with Mongoose/server.js
+++ Data Modelling with Mongoose/server.js
@@ -9,6 +9,14 @@ app.get('/' , (req , res) => {
     res.send("Express server is working ")
 })
 
+app.get('/signup' , (req, res) => {
+    res.send("SignUp to access to full content")
+})
+
+app.get('/About' , (req, res) => {
+    res.send("Wellcome to About us ")
+})
+
 app.listen(port , () => {
     console.log(`app is running on http://localhost:${port}`)
 })
