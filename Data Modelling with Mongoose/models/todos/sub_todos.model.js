a63fe16c72296d07cb0e53ade8b1675603f18e1d
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-15 11:51:49 +0500

    Storing Data Fields

--- Data Modelling with Mongoose/models/todos/sub_todos.model.js
+++ Data Modelling with Mongoose/models/todos/sub_todos.model.js
@@ -12,32 +12,32 @@ const userSchema = new mongoose.Schema(
     //     Age: Integer
     // },
     {//This is more Professional way
-        username : {
-            type : String,
-            required : true,
-            unique : true
+        username: {
+            type: String,
+            required: true,
+            unique: true
         },
-        email : {
-            type : String,
-            required : true,
-            lowecase : true
+        email: {
+            type: String,
+            required: true,
+            lowecase: true,
+            unique: true,
         },
-        password : {
-            type : String,
-            required : true,
-            unique : true,
-            uppercase : true
+        password: {
+            type: String,
+            required: true,
+            uppercase: true
         },
-        age : {
-            type : Integer,
-            required : false
+        age: {
+            type: Integer,
+            required: false
         }
     }
 )  //Schema = Blueprint / Template
 // It defines the structure of your data — what fields exist, what type they are, and any rules.
 
 //model() takes 2 parameters , kiya model banaun or kiske base pe banaon , basis defined already which is userSchema
-export const User = mongoose.model("User" , userSchema)  //Model = The Worker built from that blueprint
+export const User = mongoose.model("User", userSchema)  //Model = The Worker built from that blueprint
 // It's the actual tool you use to create, read, update, delete data in MongoDB.
 
 //When this name "User" will go in database it will be changed in "users" , it is the standarized way in MongoDB that any name you give will be changed in plural and in lowercase
