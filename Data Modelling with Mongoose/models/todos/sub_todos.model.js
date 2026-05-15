5ae0a3d885275e46dd797ba048342b83227e83c4
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-15 11:45:02 +0500

    Storing Data Fields

--- Data Modelling with Mongoose/models/todos/sub_todos.model.js
+++ Data Modelling with Mongoose/models/todos/sub_todos.model.js
@@ -19,12 +19,14 @@ const userSchema = new mongoose.Schema(
         },
         email : {
             type : String,
-            required : true
+            required : true,
+            lowecase : true
         },
         password : {
             type : String,
             required : true,
-            unique : true
+            unique : true,
+            uppercase : true
         },
         age : {
             type : Integer,
