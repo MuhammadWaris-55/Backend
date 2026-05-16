09dfa33527e9a5b0158f8dcd8342067ae59728e0
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-16 12:14:30 +0500

    Data Modelling

--- Data Modelling with Mongoose/models/todos/todo.model.js
+++ Data Modelling with Mongoose/models/todos/todo.model.js
@@ -2,7 +2,28 @@ import mongoose from 'mongoose'
 
 const todoSchema = new mongoose.Schema(
     {
-
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        }, 
+        complete: {
+            type: Boolean,
+            default: false
+        },
+        createdBy: { //In this line we r making a relation of todomodel with usermodel , it is necessary to give the ref of user same as name you defined in the model you r referring
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        subTodo: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "SubTodo"
+            }
+        ]
     }, {timestamps: true}
 )
 
