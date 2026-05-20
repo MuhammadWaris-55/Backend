51a2673beaebbf8e1b3ac33cb4444fbdc96e067f
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-20 11:19:41 +0500

    Category Model

--- Ecommerce Data Modelling/models/category.model.js
+++ Ecommerce Data Modelling/models/category.model.js
@@ -9,6 +9,6 @@ const categorySchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }
-} , {timestamps: true});
+}, { timestamps: true });
 
-export const Category = mongoose.model("Category" , categorySchema);
+export const Category = mongoose.model("Category", categorySchema);
