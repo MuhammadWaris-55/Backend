f584df592bf8239b7819b7f81296a3e4da3b4470
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-23 11:13:27 +0500

    Hospital Management Modelling

--- Hospital-management Modelling/models/hospital.model.js
+++ Hospital-management Modelling/models/hospital.model.js
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
 
 
-const hospitalSchema = new mongoose.Schema({} , {timestamps: true});
+const hospitalSchema = new mongoose.Schema({
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor"
+    },
+
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient"
+    }
+}, { timestamps: true });
 
 
-export const Hospital = mongoose.model("Hospital" , hospitalSchema);
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
--- Hospital-management Modelling/models/patient.model.js
+++ Hospital-management Modelling/models/patient.model.js
@@ -1,7 +1,53 @@
 import mongoose from "mongoose";
 
 
-const patientSchema = new mongoose.Schema({} , {timestamps: true});
+const patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    diagnosedWith: {
+        type: String,
+        required: true
+    },
+
+    address: {
+        type: String,
+        required: true
+    },
+
+    age: {
+        type: Number,
+        required: true
+    },
+
+    bloodGroup: {
+        type: String,
+        required: true
+    },
+
+    Gender: {
+        type: String,
+        enum: ["Male" , "Female"],
+        required: true
+    },
+
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor"
+    },
+
+    admittedIn: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital"
+    },
+
+    MedicalRecord: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "MedicalRecord"
+    }
+}, { timestamps: true });
 
 
-export const Patient = mongoose.model("Patient" , patientSchema);
+export const Patient = mongoose.model("Patient", patientSchema);
